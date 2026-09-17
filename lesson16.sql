4f4fa13dc71d9c345269661b05af9d9547d5201e
abhijatnav678-max <abhijatnav678@gmail.com>
2026-09-17 16:04:39 +0530

    Completed SQLBolt Lesson 16

---
+++ lesson16.sql
@@ -0,0 +1,13 @@
+
+-- SQLBolt Lesson 16
+
+--Question 1:Create a new table named Database with the following columns:
+-- Name A string (text) describing the name of the database
+-- Version A number (floating point) of the latest version of this database
+-- Download_count An integer count of the number of times this database was downloaded
+--This table has no constraints.
+
+CREATE table database(
+Name varchar not null,
+Version float ,
+Download_count integer)
