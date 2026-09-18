43fcca019d7c3f6ca7ee2cc44279388edd9091ba
abhijatnav678-max <abhijatnav678@gmail.com>
2026-09-18 10:51:52 +0530

    Completed SQLBolt Lesson 18

---
+++ lesson18.sql
@@ -0,0 +1,9 @@
+
+-- SQLBolt Lesson 18
+
+--Question 1:We've sadly reached the end of our lessons, lets clean up by removing the Movies table
+drop table movies;
+
+
+--Question 2:And drop the BoxOffice table as well
+drop table boxoffice;git 
